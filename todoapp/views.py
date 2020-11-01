5a57aeb3dfad14f0d794695192d81eae85f36756
patakib <patakibalintka@gmail.com>
2020-11-01 21:59:35 +0100

    final steps

--- todoapp/views.py
+++ todoapp/views.py
@@ -8,7 +8,6 @@ from .forms import *
 
 def index(request):
     tasks = Task.objects.all()
-
     form = TaskForm()
 
     if request.method == 'POST':
@@ -16,5 +15,26 @@ def index(request):
         if form.is_valid():
             form.save()
         return redirect('/')
-    context = {'tasks': tasks, 'form':form}
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'todoapp/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method=="POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect ('/')
+    context = {'form': form}
+    return render(request, 'todoapp/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item': item}
+    return render(request, 'todoapp/delete.html', context)
